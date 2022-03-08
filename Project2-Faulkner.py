a4eb9a4d64dac071652333bb525b572e5c1cfa48
rfaulkner100 <95778549+rfaulkner100@users.noreply.github.com>
2022-03-07 22:30:58 -0600

    Update Project2-Faulkner.py

--- Project2-Faulkner.py
+++ Project2-Faulkner.py
@@ -1,7 +1,7 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 name = "Rayna Faulkner"
-address = "42 Wallaby Way
+address = "42 Wallaby Way"
 phone number = "918-444-4444"
 SyntaxError: invalid syntax
 phonenumber = "918-640-1941"
