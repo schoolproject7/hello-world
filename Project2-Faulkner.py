31663d81e1e20782d86c9cf573fb92c694c82301
rfaulkner100 <95778549+rfaulkner100@users.noreply.github.com>
2022-03-07 22:30:19 -0600

    Update Project2-Faulkner.py

--- Project2-Faulkner.py
+++ Project2-Faulkner.py
@@ -1,8 +1,8 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 name = "Rayna Faulkner"
-address = "145 S 166 E Avenue, Tulsa, OK, 74108"
-phone number = "918-640-1941"
+address = "42 Wallaby Way
+phone number = "918-444-4444"
 SyntaxError: invalid syntax
 phonenumber = "918-640-1941"
 print("Hi there my name is,", name\n "My address is: ", address\n "and my phone number is", number\n)
